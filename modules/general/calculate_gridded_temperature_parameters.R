78538f8468d022fe88e65d5039b42ddc184a098b
Mingkai Jiang <shanyujordan512@gmail.com>
2020-08-28 11:37:48 +1000

    structural updates to alterantive code. shoud be able to replace the main code now. but check before implement.

--- modules/general/calculate_gridded_temperature_parameters.R
+++ modules/general/calculate_gridded_temperature_parameters.R
@@ -13,6 +13,12 @@ calculate_gridded_temperature_parameters <- function (sourceDir,
     ### read in dayDF
     dayDF <- read.csv("output/dateDF.csv")
     
+    ### prepare a yearDF
+    sDF <- summaryBy(s.loc~year, FUN=min, data=dayDF, na.rm=T, keep.names=T)
+    eDF <- summaryBy(e.loc~year, FUN=max, data=dayDF, na.rm=T, keep.names=T)
+    
+    yrDF <- merge(sDF, eDF, by="year")
+    
     ### prepare output storage
     out <- array(NA, c(20, 241, 4))
     
@@ -28,30 +34,139 @@ calculate_gridded_temperature_parameters <- function (sourceDir,
       ### now we have two return options:
       ### 1. calculate grow season temperature, i.e., mean temperature of months when T > 0 
       ### 2. calculate annual mean temperature
-      if (return.option == "growth") {
-        print("no code written")
+      if (return.option == "hourly") {
+          ### Tmean based on all data
+          Tmean <- rowMeans(myDF, dims = 2, na.rm=T)
+          
+          ### Tsd based on all data
+          Tsd <- apply(myDF, c(1,2), sd)
+          
+          ### calculate Topt
+          Topt <- 13.9 + 0.61 * Tmean
+          
+          ### test statistic
+          Tparam <- (Topt - Tmean) / Tsd
+          
+          ### save all to out
+          out[,,1] <- Tmean
+          out[,,2] <- Tsd
+          out[,,3] <- Topt
+          out[,,4] <- Tparam
+          
+          ### save output
+          saveRDS(out, file=paste0(destDir, "Group_", i, "_temperature_parameters.rds"))
         
+      } else if (return.option == "daily") {
+          ## create storage DF to store the daily means
+          tmpDF <- array(NA, c(20, 241, 14610))
+          
+          ## calculate daily means based on 6-hourly data
+          for (j in 1:20) {
+              for (k in 1:241) {
+                  tmpDF2 <- data.frame(myDF[j,k,], rep(c(1:14610), each=4))
+                  colnames(tmpDF2) <- c("value", "group")
+                  avgDF <- summaryBy(value~group, data=tmpDF2, keep.names=T, na.rm=T)
+                  tmpDF[j,k,] <- avgDF$value
+              }
+          }
+          
+          ### Tmean based on all data
+          Tmean <- rowMeans(tmpDF, dims = 2, na.rm=T)
+          
+          ### Tsd based on all data
+          Tsd <- apply(tmpDF, c(1,2), sd)
+          
+          ### calculate Topt
+          Topt <- 13.9 + 0.61 * Tmean
+          
+          ### test statistic
+          Tparam <- (Topt - Tmean) / Tsd
+          
+          ### save all to out
+          out[,,1] <- Tmean
+          out[,,2] <- Tsd
+          out[,,3] <- Topt
+          out[,,4] <- Tparam
+          
+          ### save output
+          saveRDS(out, file=paste0(destDir, "Group_", i, "_temperature_parameters.rds"))
+        
+      } else if (return.option == "monthly") {
+          ## create storage DF to store the monthly means
+          tmpDF <- array(NA, c(20, 241, 480))
+          
+          ## calculate monthly means based on 6-hourly data
+          for (j in 1:20) {
+              for (k in 1:241) {
+                  for (l in 1:480) {
+                      s <- dayDF[l,"s.loc"]
+                      e <- dayDF[l, "e.loc"]
+                      tmpDF[j,k,l] <- mean(myDF[j,k,s:e], na.rm=T)
+                  }
+              }
+          }
+          
+          ### Tmean based on all data
+          Tmean <- rowMeans(tmpDF, dims = 2, na.rm=T)
+          
+          ### Tsd based on all data
+          Tsd <- apply(tmpDF, c(1,2), sd)
+          
+          ### calculate Topt
+          Topt <- 13.9 + 0.61 * Tmean
+          
+          ### test statistic
+          Tparam <- (Topt - Tmean) / Tsd
+          
+          ### save all to out
+          out[,,1] <- Tmean
+          out[,,2] <- Tsd
+          out[,,3] <- Topt
+          out[,,4] <- Tparam
+          
+          ### save output
+          saveRDS(out, file=paste0(destDir, "Group_", i, "_temperature_parameters.rds"))
+          
+          
       } else if (return.option == "annual") {
-        ### Tmean based on all data
-        Tmean <- rowMeans(myDF, dims = 2, na.rm=T)
-        
-        ### Tsd based on all data
-        Tsd <- apply(myDF, c(1,2), sd)
-        
-        ### calculate Topt
-        Topt <- 13.9 + 0.61 * Tmean
-        
-        ### test statistic
-        Tparam <- (Topt - Tmean) / Tsd
-        
-        ### save all to out
-        out[,,1] <- Tmean
-        out[,,2] <- Tsd
-        out[,,3] <- Topt
-        out[,,4] <- Tparam
-        
-        saveRDS(out, file=paste0(destDir, "Group_", i, "_temperature_parameters.rds"))
-        
+          ## create storage DF to store the monthly means
+          tmpDF <- array(NA, c(20, 241, 40))
+          
+          ## calculate annual means based on 6-hourly data
+          for (j in 1:20) {
+              for (k in 1:241) {
+                  for (l in 1:40) {
+                      s <- yrDF[l,"s.loc"]
+                      e <- yrDF[l, "e.loc"]
+                      tmpDF[j,k,l] <- mean(myDF[j,k,s:e], na.rm=T)
+                  }
+              }
+          }
+          
+          ### Tmean based on all data
+          Tmean <- rowMeans(tmpDF, dims = 2, na.rm=T)
+          
+          ### Tsd based on all data
+          Tsd <- apply(tmpDF, c(1,2), sd)
+          
+          ### calculate Topt
+          Topt <- 13.9 + 0.61 * Tmean
+          
+          ### test statistic
+          Tparam <- (Topt - Tmean) / Tsd
+          
+          ### save all to out
+          out[,,1] <- Tmean
+          out[,,2] <- Tsd
+          out[,,3] <- Topt
+          out[,,4] <- Tparam
+          
+          ### save output
+          saveRDS(out, file=paste0(destDir, "Group_", i, "_temperature_parameters.rds"))
+          
+          
+      } else {
+          print("no option")
       } # if statement for return.option
       
     } # for statement for file loop
--- run_program_alt.R
+++ run_program_alt.R
@@ -29,10 +29,10 @@ source("prepare.R")
 ################## Basic code to process temperature data #################################
 #### Structure:
 #### 1. prepare storage DF
-#### 2. loop through custom-defined year, 
-####    and perform monthly mean, sd and n calculation, based on 6-hourly data
-#### 3. prepare annual storage DF
-#### 4. prepare sea surface mask
+#### 2. convert data from per month to per region
+#### 3. calculate Tmean, Tsd based on defined period (hourly, daily, monthly, or annual)
+#### 4. merge all data
+#### 5. Plotting
 
 ### 1. create a date DF 
 ### run once and it's done
@@ -56,34 +56,127 @@ prepare_ERA_INTERIM_dataset_and_split(sourceDir="/Volumes/TOSHIBAEXT/era_interim
 ###                 annual - based on annual mean to calculate mean and sd
 ### for now, don't split into all data and growth period data, but will need to come back!
 calculate_gridded_temperature_parameters(sourceDir="/Volumes/TOSHIBAEXT/era_interim/processed/", 
-                                         destDir="output/splitted/",
+                                         destDir="output/splitted/hourly/",
+                                         return.option="hourly")
+
+calculate_gridded_temperature_parameters(sourceDir="/Volumes/TOSHIBAEXT/era_interim/processed/", 
+                                         destDir="output/splitted/daily/",
+                                         return.option="daily")
+
+calculate_gridded_temperature_parameters(sourceDir="/Volumes/TOSHIBAEXT/era_interim/processed/", 
+                                         destDir="output/splitted/monthly/",
+                                         return.option="monthly")
+
+calculate_gridded_temperature_parameters(sourceDir="/Volumes/TOSHIBAEXT/era_interim/processed/", 
+                                         destDir="output/splitted/annual/",
                                          return.option="annual")
 
+
+
 ### 4. Merge the caluclated temperature data, then remove sea surface area mask
-landDF <- merge_splitted_files_and_remove_sea_surface(sourceDir="output/splitted/", 
-                                                      destDir="output/merged/",
-                                                      return.option="annual")
+landDF1 <- merge_splitted_files_and_remove_sea_surface(sourceDir="output/splitted/hourly/", 
+                                                       destDir="output/merged/hourly/",
+                                                       return.option="hourly")
+
+landDF2 <- merge_splitted_files_and_remove_sea_surface(sourceDir="output/splitted/daily/", 
+                                                       destDir="output/merged/daily/",
+                                                       return.option="daily")
+
+landDF3 <- merge_splitted_files_and_remove_sea_surface(sourceDir="output/splitted/monthly/", 
+                                                       destDir="output/merged/monthly/",
+                                                       return.option="monthly")
+
+landDF4 <- merge_splitted_files_and_remove_sea_surface(sourceDir="output/splitted/annual/", 
+                                                       destDir="output/merged/annual/",
+                                                       return.option="annual")
 
 ### 5. plot
-plotDF = landDF
+## hourly
+plotDF = landDF1
 sd.filter.option = "no.filter"
 outdir = "output/alternative/"
-outname = "diurnal_annual"
-prepare_figure_output(plotDF = landDF,
+outname = "hourly_all"
+prepare_figure_output(plotDF = landDF1,
                       sd.filter.option = "no.filter",
                       outdir = "output/alternative/", 
-                      outname = "diurnal_annual")
+                      outname = "hourly_all")
 
-make_paper_quality_figures(plotDF = landDF,
+make_paper_quality_figures(plotDF = landDF1,
                            sd.filter.option = "no.filter",
                            outdir = "output/alternative/", 
-                           outname = "diurnal_annual")
+                           outname = "hourly_all")
 
 
-make_paper_quality_figures_ecosystem(plotDF = landDF,
+make_paper_quality_figures_ecosystem(plotDF = landDF1,
                                      sd.filter.option = "no.filter",
                                      outdir = "output/alternative/", 
-                                     outname = "diurnal_annual")
+                                     outname = "hourly_all")
+
+
+## daily
+plotDF = landDF2
+sd.filter.option = "no.filter"
+outdir = "output/alternative/"
+outname = "daily_all"
+prepare_figure_output(plotDF = landDF2,
+                      sd.filter.option = "no.filter",
+                      outdir = "output/alternative/", 
+                      outname = "daily_all")
+
+make_paper_quality_figures(plotDF = landDF2,
+                           sd.filter.option = "no.filter",
+                           outdir = "output/alternative/", 
+                           outname = "daily_all")
+
+
+make_paper_quality_figures_ecosystem(plotDF = landDF2,
+                                     sd.filter.option = "no.filter",
+                                     outdir = "output/alternative/", 
+                                     outname = "daily_all")
+
+
+## monthly
+plotDF = landDF3
+sd.filter.option = "no.filter"
+outdir = "output/alternative/"
+outname = "monthly_all"
+prepare_figure_output(plotDF = landDF3,
+                      sd.filter.option = "no.filter",
+                      outdir = "output/alternative/", 
+                      outname = "monthly_all")
+
+make_paper_quality_figures(plotDF = landDF3,
+                           sd.filter.option = "no.filter",
+                           outdir = "output/alternative/", 
+                           outname = "monthly_all")
+
+
+make_paper_quality_figures_ecosystem(plotDF = landDF3,
+                                     sd.filter.option = "no.filter",
+                                     outdir = "output/alternative/", 
+                                     outname = "monthly_all")
+
+
+## annual
+plotDF = landDF4
+sd.filter.option = "no.filter"
+outdir = "output/alternative/"
+outname = "annual_all"
+prepare_figure_output(plotDF = landDF4,
+                      sd.filter.option = "no.filter",
+                      outdir = "output/alternative/", 
+                      outname = "annual_all")
+
+make_paper_quality_figures(plotDF = landDF4,
+                           sd.filter.option = "no.filter",
+                           outdir = "output/alternative/", 
+                           outname = "annual_all")
+
+
+make_paper_quality_figures_ecosystem(plotDF = landDF4,
+                                     sd.filter.option = "no.filter",
+                                     outdir = "output/alternative/", 
+                                     outname = "annual_all")
 
 ######################################## End basic code ##################################
 ##########################################################################################
